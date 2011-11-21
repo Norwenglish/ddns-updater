bbde5c84561753472192d701a09912c30d179ed0
Dmitriy <dmitriy.trochshenko@gmail.com>
2011-11-21 23:11:33 +0600

    Request file from dns instead of storing it in file

--- ddns-updater.py
+++ ddns-updater.py
@@ -5,8 +5,8 @@ from urllib import urlopen
 import socket
 import subprocess
 
-ip = '127.0.0.1'
-PREV_UPDATE_FILE = '/Users/mac/.prev_ddns_update'
+
+host = 'trebuha.dyndns.org'
 def check_ip(ip):
 	try:
 		socket.inet_aton(ip)
@@ -16,24 +16,18 @@ def check_ip(ip):
 	else:
 		return ip
 
-def get_ip_from_file(PREV_UPDATE_FILE):
+def get_ip_from_dns(host):
+	DIG_CMD = ['/usr/bin/dig', '+short', host]
 	try:
-		prev_update_file = open(PREV_UPDATE_FILE)
+		 ip = subprocess.check_output(DIG_CMD).rstrip()
+	except CalledProcessError as e:
+		print "Dig error: %s" % e
+		exit(0)
 	except Exception, e:
-		print "Couldn't open file: %s" % e
-		ip = get_current_ip()
-		if ip:
-			update_file(PREV_UPDATE_FILE, ip)
+		print "Couldn't get launch dig: %s" % e
 		exit(0)
 	else:
-		try:
-			ip = prev_update_file.readline()
-		except Exception, e:
-			print "Couldn't get ip from file: %s" % e
-			ip = Null
-		else:
-			prev_update_file.close()
-			return check_ip(ip)
+		return check_ip(ip)
 
 def get_current_ip():
 	try:
@@ -44,36 +38,25 @@ def get_current_ip():
 	else:
 		return check_ip(ip)
 
-def update_file(PREV_UPDATE_FILE, ip):
-	try:
-		file_ip = open(PREV_UPDATE_FILE, 'w')
-		file_ip.write(ip)
-		file_ip.close()
-	except Exception, e:
-		print "Could not update file: %s " % e
-	return 1
-
 def update_ddns(ip):
 	UPDATE_CMD = ['/opt/local/bin/ez-ipupdate', '-c', '/Users/mac/.ddns', '-a', '%s' % ip]
 	try:
-		#print "trying to update ddns"
 		fnull = open(os.devnull, 'w')
-		if not subprocess.call(UPDATE_CMD, stdout = fnull):
-			#print "Updated successfully"
-			pass
-		else:
-			print "ez-ipupdate exited abnormally"
+		subprocess.check_call(UPDATE_CMD, stdout = fnull)
 		fnull.close()
+		return 1
+	except CalledProcessError as e:
+		print "ez-ipupdate exited abnormally: %s" % e
+		exit(0)
 	except Exception, e:
 		print "Couldn't update ddns: %s" % e
 		exit(0)
 
 def main():
-	last_ip = get_ip_from_file(PREV_UPDATE_FILE)
-	cur_ip = get_current_ip()
+	last_ip = get_ip_from_dns(host)
+	cur_ip 	= get_current_ip()
 	if last_ip != cur_ip:
 		update_ddns(cur_ip)
-		update_file(PREV_UPDATE_FILE, cur_ip)
 	else:
 		#print "ip not changed"
 		pass
